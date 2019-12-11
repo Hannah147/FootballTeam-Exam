7e9d18968847a4492245f76c216710baee123d43
Hannah Byrne <S00189764@mail.itsligo.ie>
2019-12-11 09:52:24 +0000

    Created Properties

--- FootballTeam/Player.cs
+++ FootballTeam/Player.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace FootballTeam
 {
+    //public enum Position { Goalkeeper, Defender, Midfielder, Forward}
     class Player
     {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public Position Position { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        //public int Age { get; set; }
+
+        private int _age;
+
+        public int Age
+        {
+            get
+            {
+                _age = DateTime.Now.Year - DateOfBirth.Year;
+                if(DateOfBirth.DayOfYear > DateTime.Now.DayOfYear)
+                    _age--;
+                    return _age;
+            }
+            
+        }
     }
 }
